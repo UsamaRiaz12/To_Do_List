24b77aa2aa616c9e905dc02878b546f6ad4c0ae8
UsamaRiaz12 <63581461+UsamaRiaz12@users.noreply.github.com>
2023-04-30 21:33:05 +0500

    Add files via upload
    This Django-based To-Do List with User Authentication web application enables users to securely log in and manage their tasks. Its development demonstrates the developer's commitment to enhancing their skills and creating valuable software.

---
+++ todo_list/base/models.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+class Task(models.Model):
+    user =models.ForeignKey(
+          User,on_delete=models.CASCADE ,null=True ,blank=True)
+    
+    tittle=models.CharField(max_length=200)
+    description =models.TextField(null=True ,blank=True)
+    complete=models.BooleanField(default=True)
+    created =models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.tittle
+    
+
+    class Meta:
+        ordering=['complete']
+    
+
